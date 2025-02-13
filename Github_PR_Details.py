3eca3e0fe5ccb0baa3f2c9135946e455c356429c
pkishore449 <117738837+pkishore449@users.noreply.github.com>
2025-02-13 22:46:22 +0530

    Update Github_PR_Details.py

--- Github_PR_Details.py
+++ Github_PR_Details.py
@@ -7,4 +7,14 @@ output=access.json()
 print('ID-----------USER')
 for i in range(len(output)):
     print(output[i]["id"],"---",output[i]["user"]['login'])
+**************************************************************************************
+import requests
+
+url='https://api.github.com/repos/iam-veeramalla/python-for-devops/pulls'
+
+hit_url=requests.get(url)
+output=hit_url.json()
+for i in range(len(output)):
+    print("user id:",output[i]["id"],"-user name:",output[i]["user"]['login'],"-created date:",output[i]["created_at"])
+
     
